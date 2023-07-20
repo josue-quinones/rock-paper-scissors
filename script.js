8909690020f02e7e72f177cf427203ae561c5a45
Josue Quinones <74983067+josue-quinones@users.noreply.github.com>
2023-07-20 16:03:29 -0400

    Add function getComputerChoice
    Function randomly returns an option for the computer

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+choices = ["ROCK","PAPER","SCISSORS"];
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
+}
+
+// console.log(getComputerChoice());
